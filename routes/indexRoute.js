82106e972858bb11e1598a8f5d073f6ecc7ec759
thongster <106802301+thongster@users.noreply.github.com>
2026-01-08 23:27:04 +0700

    added messages array

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,6 +1,23 @@
 const { Router } = require('express');
-
 const indexRoute = Router();
+
+const messages = [
+  {
+    text: 'Just hit level 30 and finally became a Cleric! Time to grind zombie mushrooms.',
+    user: 'OGcleric',
+    added: new Date(),
+  },
+  {
+    text: 'Anyone Zakum tonight? Need a helm for my Dexless Sin.',
+    user: 'xOPsinx',
+    added: new Date(),
+  },
+  {
+    text: "Spent 2 hours at Henesys Hunting Ground ks'ing noobs. Classic Maple.",
+    user: 'IlikeArrow',
+    added: new Date(),
+  },
+];
 
 indexRoute.get('/', (req, res) => {
   res.send('This is the index page');
